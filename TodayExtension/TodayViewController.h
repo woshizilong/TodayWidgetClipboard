c628ad55065a4af196d3e4184aa1f87f5c99ef54
poolqf <polkintana@gmail.com>
2014-10-03 19:41:41 +0200

    Implemented Clipboard

---
+++ TodayExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayExtension
+//
+//  Created by Pol Quintana on 03/10/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
